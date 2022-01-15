74171471d2f2eb3419e2855aae847cf347b7237c
muberraozmen <babam1harika>
2022-01-15 09:50:21 -0500

    initial commit

--- cedar.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --nodes=1
-#SBATCH --gres=gpu:1              # Number of GPUs (per node)
-#SBATCH --mem=8                  # memory (per node)
-#SBATCH --time=0-1:00
-#SBATCH --output=out.log          # output file
-#SBATCH --error=err.log           # error file
-
-python3.8 -u main.py
